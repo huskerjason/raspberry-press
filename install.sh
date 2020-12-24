eac0685832a226423a8dbba1749e1542a0b6aea8
Husker <61723625+huskerjason@users.noreply.github.com>
2020-12-23 22:48:13 -0500

    wp 5.6

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ cd $DIR_SOURCE
 . functs/update.sh
 . functs/lamp.sh
 . functs/phpmyadmin-5.0.4.sh
-. functs/wordpress-5.5.3.sh
+. functs/wordpress-5.6.sh
 . functs/wp-permissions.sh
 . functs/display-stuff.sh
 . functs/config-wait.sh
