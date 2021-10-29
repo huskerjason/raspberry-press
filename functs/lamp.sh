8eb8526a6f5c55f8dc3d76ffc3ea1fc34110b9a8
Husker <61723625+huskerjason@users.noreply.github.com>
2021-10-29 09:42:30 -0400

    php test 4

--- functs/lamp.sh
+++ functs/lamp.sh
@@ -22,7 +22,7 @@ function lamp {
 	whiptail --title 'Progress' --infobox "$text" 18 76 --backtitle 'Famous 10 Minute Install'
 
 #	sudo DEBIAN_FRONTEND=noninteractive apt-get install apache2 mariadb-server php7.4 php7.4-{bcmath,imagick,mysql,xml,mbstring,zip,curl} pwgen unzip -y < /dev/null > /dev/null
-	sudo DEBIAN_FRONTEND=noninteractive apt install apache2 mariadb-server php php-{bcmath,imagick,mysql,xml,mbstring,zip,curl} pwgen unzip -y < /dev/null > /dev/null
+	sudo DEBIAN_FRONTEND=noninteractive apt-get install apache2 mariadb-server php php-{bcmath,imagick,mysql,xml,mbstring,zip,curl} pwgen unzip -y < /dev/null > /dev/null
 
 	sudo cp files/apache2.conf /etc/apache2/apache2.conf
 	sudo a2enmod rewrite > /dev/null
