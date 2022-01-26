e08165bea29ee3ae938a693f46fb0f8044ee9ce0
Husker <61723625+huskerjason@users.noreply.github.com>
2022-01-26 10:48:49 -0500

    WordPress 5.9

---
+++ files/temp/rename.bat
@@ -0,0 +1,14 @@
+ren twentyeleven*.zip twentyeleven.zip
+ren twentyfifteen*.zip twentyfifteen.zip
+ren twentyfourteen*.zip twentyfourteen.zip
+ren twentynineteen*.zip twentynineteen.zip
+ren twentyseventeen*.zip twentyseventeen.zip
+ren twentysixteen*.zip twentysixteen.zip
+ren twentyten*.zip twentyten.zip
+ren twentythirteen*.zip twentythirteen.zip
+ren twentytwelve*.zip twentytwelve.zip
+ren twentytwenty*.zip twentytwenty.zip
+ren twentytwentyone*.zip twentytwentyone.zip
+ren twentytwentytwo*.zip twentytwentytwo.zip
+ren phpMyAdmin*.tar.gz phpMyAdmin.tar.gz
+ren wordpress*.tar.gz wordpress.tar.gz
